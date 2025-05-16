855b56ed09aeb05616e33888d59f031c28191f59
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-16 09:35:57 -0300

    Up to video 61

--- 01-express-tutorial/app.js
+++ 01-express-tutorial/app.js
@@ -1,7 +1,44 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// Get all files
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
-  console.log('The user hit the server');
+  const url = req.url;
+  // Home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(homePage);
+    res.end();
+  }
+  // Styles
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyles);
+    res.end();
+  }
+  // Logo/images
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homeImage);
+    res.end();
+  }
+  // Logic
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
+    res.end();
+  }
+  // Logo
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>404 Page</h1>');
+    res.end();
+  }
 });
 
 server.listen(8000);
