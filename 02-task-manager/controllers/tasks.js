2887033cc78cbe99a9f42d6a46ab41d89e7a4108
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-22 12:01:48 -0300

    Up to 115

--- 02-task-manager/controllers/tasks.js
+++ 02-task-manager/controllers/tasks.js
@@ -1,28 +1,69 @@
+// Imports
+const Task = require('../models/Task');
+
 // Controllers
-const getAllTasks = (req, res) => {
-  res.status(200).json({ success: true, data: { message: 'GET all tasks' } });
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res
+      .status(200)
+      .json({ success: true, message: 'GET all tasks', data: { tasks } });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
-const createTask = (req, res) => {
-  res.status(201).json({
-    success: true,
-    data: { message: 'POST task', contents: req.body },
-  });
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({
+      success: true,
+      message: 'POST task',
+      data: { contents: task },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
-const getSingleTask = (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { message: 'GET single task', id: req.params.id },
-  });
+const getSingleTask = async (req, res) => {
+  try {
+    // Added "taskID" alias when destructuring for clarity
+    const { id: taskID } = req.params;
+    // "findOne" will store the whole task object inside "task"
+    const task = await Task.findOne({ _id: taskID });
+    // If the id's syntax is correct but doesn't exist
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'GET single task',
+      data: { task },
+    });
+  } catch (error) {
+    // If the id's syntax is wrong or any other issue exists
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 const updateTask = (req, res) => {
-  res.status(200).json({ success: true, data: { message: 'PATCH task' } });
+  res.status(200).json({ success: true, message: 'PATCH task', data: {} });
 };
 
 const deleteTask = (req, res) => {
-  res.status(200).json({ success: true, data: { message: 'DELETE task' } });
+  res.status(200).json({ success: true, message: 'DELETE task', data: {} });
 };
 
 // Exports
