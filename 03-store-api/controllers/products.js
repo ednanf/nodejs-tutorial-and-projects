d6b7be825a6a5802bca3efe46e23937c4efa133b
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-27 14:39:54 -0300

    Up to 140

--- 03-store-api/controllers/products.js
+++ 03-store-api/controllers/products.js
@@ -1,9 +1,19 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res, next) => {
-  res.status(200).json({ data: 'products testing route' });
+  const products = await Product.find({ name: 'vase table' });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getProducts = async (req, res, next) => {
-  res.status(200).json({ data: 'products route' });
+  // We extract the properties to avoid unwanted query parameters
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getProducts };
