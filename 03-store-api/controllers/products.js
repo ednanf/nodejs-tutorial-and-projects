bfb8e06d03f7e85124abf01794cd125fb97d29f6
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-28 10:25:18 -0300

    Up to 150

--- 03-store-api/controllers/products.js
+++ 03-store-api/controllers/products.js
@@ -6,13 +6,58 @@ const getAllProductsStatic = async (req, res, next) => {
 };
 
 const getProducts = async (req, res, next) => {
-  // We extract the properties to avoid unwanted query parameters
-  const { featured } = req.query;
+  // Extract the properties to avoid unwanted query parameters
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|=|<=|>=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`,
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+  let result = Product.find(queryObject);
+  // Sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  // Fields
+  if (fields) {
+    const fieldList = fields.split(',').join(' ');
+    result = result.select(fieldList);
+  }
+  // Pagination (limit + skip)
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
