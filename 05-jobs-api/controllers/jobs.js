35962e7d508bcd76ce6b45e92b42140781a99258
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-05-30 10:45:00 -0300

    Up to 171

---
+++ 05-jobs-api/controllers/jobs.js
@@ -0,0 +1,27 @@
+const getJobs = async (req, res, next) => {
+  res.send('get jobs');
+};
+
+const createJob = async (req, res, next) => {
+  res.send('create job');
+};
+
+const getJob = async (req, res, next) => {
+  res.send('get job');
+};
+
+const patchJob = async (req, res, next) => {
+  res.send('patch job');
+};
+
+const deleteJob = async (req, res, next) => {
+  res.send('delete job');
+};
+
+module.exports = {
+  getJobs,
+  createJob,
+  getJob,
+  patchJob,
+  deleteJob,
+};
