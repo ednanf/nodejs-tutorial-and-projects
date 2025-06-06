48e86e4599b77dcda4ded3779267cfae441711c4
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-06-06 11:22:49 -0300

    Up to 194

--- 05-jobs-api/controllers/jobs.js
+++ 05-jobs-api/controllers/jobs.js
@@ -1,21 +1,58 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { BadRequestError, NotFoundError } = require('../errors');
+const Job = require('../models/Job');
+
 const getJobs = async (req, res, next) => {
-  res.send('get jobs');
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 const createJob = async (req, res, next) => {
-  res.json(req.user);
+  // First we create a new property in the request, attributing the value of userId (which is the current logged in user)
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 const getJob = async (req, res, next) => {
-  res.send('get job');
+  const userId = req.user.userId;
+  const jobId = req.params.id;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const patchJob = async (req, res, next) => {
-  res.send('patch job');
+  const userId = req.user.userId;
+  const jobId = req.params.id;
+  const company = req.body.company;
+  const position = req.body.position;
+  // Prevent someone editing and leaving a field empty
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or position fields cannot be empty');
+  }
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true },
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res, next) => {
-  res.send('delete job');
+  const userId = req.user.userId;
+  const jobId = req.params.id;
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
