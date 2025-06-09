4ec4244dd914b505a924406b4c89575c6d030d45
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-06-09 08:44:46 -0300

    Up to 212

--- 05-jobs-api/app.js
+++ 05-jobs-api/app.js
@@ -1,8 +1,12 @@
 require('@dotenvx/dotenvx').config();
 require('express-async-errors');
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 const express = require('express');
-const app = express();
+const app = require('express')();
 
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -17,7 +21,17 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // middleware
+app.set('trust proxy', 1); // needed for rateLimiter + Heroku
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // 100 requests per windowMs
+  }),
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
